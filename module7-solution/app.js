aec7a969c4810fb0b9cc2d764065260ed63cb2ca
Leo Smith <minismitty2001@yahoo.com>
2017-03-12 21:21:18 -0400

    Added Module7-solution

---
+++ module7-solution/app.js
@@ -0,0 +1,96 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+// inject service to protect against minification
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var list = this;
+  list.items = ShoppingListCheckOffService.getToBuyItems();
+
+  list.moveItem = function (itemIndex) {
+    ShoppingListCheckOffService.moveItem(itemIndex);
+  };
+
+  list.isEmpty = function () {
+    return ShoppingListCheckOffService.isToBuyEmpty();
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var list = this;
+  list.items = ShoppingListCheckOffService.getAlreadyBoughtItems();
+
+  list.isEmpty = function () {
+    return ShoppingListCheckOffService.isAlreadyBoughtEmpty();
+  };
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  var toBuyItems = [
+    {
+      name: "Milk",
+      quantity: 2,
+      pricePerItem: 2
+    },
+    {
+      name: "Donuts",
+      quantity: 12,
+      pricePerItem: .25
+    },
+    {
+      name: "Cookies",
+      quantity: 24,
+      pricePerItem: .1
+    },
+    {
+      name: "Chocolate",
+      quantity: 5,
+      pricePerItem: 1
+    },
+    {
+      name: "Strawberries",
+      quantity: 5,
+      pricePerItem: .75
+    }
+  ];
+
+  // initially blank
+  var alreadyBoughtItems = [];
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  }
+
+  service.isToBuyEmpty = function () {
+    return (toBuyItems.length == 0);
+  }
+
+  service.getAlreadyBoughtItems = function () {
+    return alreadyBoughtItems;
+  }
+
+  service.isAlreadyBoughtEmpty = function () {
+    return (alreadyBoughtItems.length == 0);
+  }
+
+  service.moveItem = function (itemIndex) {
+    // save item to move
+    var itemToMove = toBuyItems[itemIndex];
+
+    // remove item from toBuyItems
+    toBuyItems.splice(itemIndex, 1);
+
+    // push item removed to alreadyBoughtItems
+    alreadyBoughtItems.push(itemToMove);
+  }
+}
+
+})();
